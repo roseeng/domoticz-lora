beb5ed36cee459afdc9e868dc7afa61f4f795b57
Göran Roseen <goran@roseen.se>
2022-08-12 14:43:49 +0200

    Ok, now we have working base and beach nodes, communicating

--- ttgo-base/src/main.cpp
+++ ttgo-base/src/main.cpp
@@ -3,7 +3,6 @@
 #include <LoRa.h>
 #include <Wire.h>  
 #include "SSD1306.h" 
-#include "images.h"
 
 #define SCK     5    // GPIO5  -- SX1278's SCK
 #define MISO    19   // GPIO19 -- SX1278's MISnO
@@ -13,27 +12,29 @@
 #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
 #define BAND  868E6
 
-unsigned int counter = 0;
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RST 16
 
-SSD1306 display(0x3c, 4, 15);
+SSD1306 display(0x3c, OLED_SDA, OLED_SCL);
+
+unsigned int counter = 0;
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
 
- 
-
 void setup() {
-  pinMode(16,OUTPUT);
-  pinMode(2,OUTPUT);
+  pinMode(OLED_RST, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   
-  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  digitalWrite(OLED_RST, LOW);    // set GPIO16 low to reset OLED
   delay(50); 
-  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
+  digitalWrite(OLED_RST, HIGH); // while OLED is running, must set GPIO16 in high
   
   Serial.begin(115200);
   while (!Serial);
   Serial.println();
-  Serial.println("LoRa Sender Test");
+  Serial.println("Domoticz-LoRa Base ver 0.1");
   
   SPI.begin(SCK,MISO,MOSI,SS);
   LoRa.setPins(SS,RST,DI0);
@@ -48,7 +49,7 @@ void setup() {
   display.flipScreenVertically();  
   display.setFont(ArialMT_Plain_10);
    
-  delay(1500);
+  delay(1500);  
 }
 
 void loop() {
@@ -56,8 +57,9 @@ void loop() {
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_10);
   
-  display.drawString(0, 0, "Sending packet: ");
-  display.drawString(90, 0, String(counter));
+  display.drawString(0, 0, "-- LoRa Base node --");
+  display.drawString(0, 15, "Sending packet: ");
+  display.drawString(90, 15, String(counter));
   Serial.println(String(counter));
   display.display();
 
@@ -68,9 +70,9 @@ void loop() {
   LoRa.endPacket();
 
   counter++;
-  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
 
--- ttgo-beachclient/src/main.cpp
+++ ttgo-beachclient/src/main.cpp
@@ -3,7 +3,6 @@
 #include <LoRa.h>
 #include <Wire.h>  
 #include "SSD1306.h" 
-#include "images.h"
 
 #define SCK     5    // GPIO5  -- SX1278's SCK
 #define MISO    19   // GPIO19 -- SX1278's MISnO
@@ -13,64 +12,67 @@
 #define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
 #define BAND  868E6
 
-unsigned int counter = 0;
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RST 16
 
-SSD1306 display(0x3c, 4, 15);
+SSD1306 display(0x3c, OLED_SDA, OLED_SCL);
+
 String rssi = "RSSI --";
 String packSize = "--";
 String packet ;
 
- 
+void loraData(){
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 0, "-- LoRa Beach node --"); 
+  display.drawString(0 , 15 , "Received "+ packSize + " bytes");
+  display.drawStringMaxWidth(0 , 26 , 128, packet);
+  display.drawString(0, 38, rssi);  
+  display.display();
+}
+
+void cbk(int packetSize) {
+  packet ="";
+  packSize = String(packetSize,DEC);
+  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+  loraData();
+}
 
 void setup() {
-  pinMode(16,OUTPUT);
-  pinMode(2,OUTPUT);
-  
-  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);    // set GPIO16 low to reset OLED
   delay(50); 
-  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
+  digitalWrite(OLED_RST, HIGH); // while OLED is running, must set GPIO16 in high、
   
   Serial.begin(115200);
   while (!Serial);
   Serial.println();
-  Serial.println("LoRa Sender Test");
+  Serial.println("Domoticz-LoRa Beach node ver 0.1");
   
   SPI.begin(SCK,MISO,MOSI,SS);
-  LoRa.setPins(SS,RST,DI0);
-  if (!LoRa.begin(868E6)) {
+  LoRa.setPins(SS,RST,DI0);  
+  if (!LoRa.begin(BAND)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
+
   //LoRa.onReceive(cbk);
-//  LoRa.receive();
+  LoRa.receive();
   Serial.println("init ok");
   display.init();
   display.flipScreenVertically();  
   display.setFont(ArialMT_Plain_10);
-   
+  
   delay(1500);
 }
 
 void loop() {
-  display.clear();
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.setFont(ArialMT_Plain_10);
-  
-  display.drawString(0, 0, "Sending packet: ");
-  display.drawString(90, 0, String(counter));
-  Serial.println(String(counter));
-  display.display();
-
-  // send packet
-  LoRa.beginPacket();
-  LoRa.print("hello kotte ");
-  LoRa.print(counter);
-  LoRa.endPacket();
-
-  counter++;
-  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) { 
+    cbk(packetSize);  
+  }
+  delay(10);
 }
-
