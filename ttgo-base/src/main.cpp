79e062423aba00181b9a8b0a304f2e2836277483
Göran Roseen <goran.roseen@nobelprize.org>
2023-09-19 09:28:39 +0200

    Fixade core dumpen som kom om man använda interrupt i Lora receive. Tydligen får man inte använda interrupt i en interrupt handler :-)

--- ttgo-base/src/main.cpp
+++ ttgo-base/src/main.cpp
@@ -11,25 +11,7 @@
   Koden är skriven för en Lilygo TTGO Lora32 V2.1_1.6
 */
 
-/*
-Gamla värden?
-
-#define SCK     5    // GPIO5  -- SX1278's SCK
-#define MISO    19   // GPIO19 -- SX1278's MISnO
-#define MOSI    27   // GPIO27 -- SX1278's MOSI
-#define SS      18   // GPIO18 -- SX1278's CS
-#define RST     14   // GPIO14 -- SX1278's RESET
-#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
-*/
 #define BAND  868E6
-/*
-#define OLED_SDA 4
-#define OLED_SCL 15
-#define OLED_RST 16
-*/
-
-#define OLED_SDA MY_DISPLAY_SDA
-#define OLED_SCL MY_DISPLAY_SCL
 
 SSD1306 display(0x3c, OLED_SDA, OLED_SCL);
 
@@ -39,29 +21,28 @@ String packSize = "--";
 String packet ;
 
 void setup() {
-  //pinMode(OLED_RST, OUTPUT);
-  //pinMode(LED_BUILTIN, OUTPUT);
   pinMode(HAS_LED, OUTPUT);
+  digitalWrite(HAS_LED, LOW);  
   
-  digitalWrite(OLED_RST, LOW);    // set GPIO16 low to reset OLED
-  delay(50); 
-  digitalWrite(OLED_RST, HIGH); // while OLED is running, must set GPIO16 in high
+  if (OLED_RST != NOT_A_PIN)
+  {
+    digitalWrite(OLED_RST, LOW);    // set GPIO16 low to reset OLED
+    delay(50); 
+    digitalWrite(OLED_RST, HIGH); // while OLED is running, must set GPIO16 in high、
+  }
   
   Serial.begin(115200);
   while (!Serial);
   Serial.println();
   Serial.println("Domoticz-LoRa Base ver 0.1");
   
-//  SPI.begin(SCK,MISO,MOSI,SS);
-//  LoRa.setPins(SS,RST,DI0);
   SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
   LoRa.setPins(LORA_CS, LORA_RST, LORA_IRQ);
   if (!LoRa.begin(BAND)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
-  //LoRa.onReceive(cbk);
-//  LoRa.receive();
+
   Serial.println("init ok");
   display.init();
   display.flipScreenVertically();  
@@ -88,9 +69,12 @@ void loop() {
   LoRa.endPacket();
 
   counter++;
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  /*
+  digitalWrite(HAS_LED, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(HAS_LED, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
+  */
+   delay(2000);
 }
 
--- ttgo-beachclient/src/main.cpp
+++ ttgo-beachclient/src/main.cpp
@@ -10,25 +10,7 @@
   Koden är skriven för en Lilygo TTGO Lora32 V2.1_1.6
 */
 
-/*
-Gamla värden?
-
-#define SCK     5    // GPIO5  -- SX1278's SCK
-#define MISO    19   // GPIO19 -- SX1278's MISnO
-#define MOSI    27   // GPIO27 -- SX1278's MOSI
-#define SS      18   // GPIO18 -- SX1278's CS
-#define RST     14   // GPIO14 -- SX1278's RESET
-#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
-*/
 #define BAND  868E6
-/*
-#define OLED_SDA 4
-#define OLED_SCL 15
-#define OLED_RST 16
-*/
-
-#define OLED_SDA MY_DISPLAY_SDA
-#define OLED_SCL MY_DISPLAY_SCL
 
 SSD1306 display(0x3c, OLED_SDA, OLED_SCL);
 
@@ -48,33 +30,34 @@ void refreshScreen(){
 }
 
 void onReceive(int packetSize) {
-  Serial.println("onReceive");
-
+  digitalWrite(HAS_LED, HIGH);  
+  
   packet ="";
   packSize = String(packetSize,DEC);
   for (int i = 0; i < packetSize; i++) { 
     packet += (char) LoRa.read(); 
   }
   rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
-  refreshScreen();
 }
 
 void setup() {
-  //pinMode(OLED_RST, OUTPUT);
-  digitalWrite(OLED_RST, LOW);    // set GPIO16 low to reset OLED
-  delay(50); 
-  digitalWrite(OLED_RST, HIGH); // while OLED is running, must set GPIO16 in high、
-  
+  pinMode(HAS_LED, OUTPUT);
+  digitalWrite(HAS_LED, LOW);  
+
+  if (OLED_RST != NOT_A_PIN)
+  {
+    digitalWrite(OLED_RST, LOW);    // set GPIO16 low to reset OLED
+    delay(50); 
+    digitalWrite(OLED_RST, HIGH); // while OLED is running, must set GPIO16 in high、
+  }
+
   Serial.begin(115200);
   while (!Serial);
   delay(1500);
 
-  Serial.println();
-  Serial.println("Domoticz-LoRa Beach node ver 0.1");
+  Serial.println();+
+  Serial.println("Domoticz-LoRa Beach node ver 0.2");
   
-//  pinMode(DI0, INPUT);
-//  SPI.begin(SCK, MISO, MOSI, SS);
-//  LoRa.setPins(SS, RST, DI0);   
   pinMode(LORA_IRQ, INPUT);
   SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_CS);
   LoRa.setPins(LORA_CS, LORA_RST, LORA_IRQ);  
@@ -95,18 +78,22 @@ void setup() {
   display.display();
 
 // For interrupt-driven:
-//  LoRa.onReceive(onReceive);
-//  LoRa.receive();
+  LoRa.onReceive(onReceive);
+  LoRa.receive();
   
   Serial.println("init ok - listening...");
 }
 
 void loop() {
   // For polling:
-  int packetSize = LoRa.parsePacket();
+/*  int packetSize = LoRa.parsePacket();
   if (packetSize) { 
     onReceive(packetSize);  
   }
   delay(10);
-  
+  */
+  refreshScreen();
+  delay(1000);
+  digitalWrite(HAS_LED, LOW);  
+//  delay(1000);
 }
